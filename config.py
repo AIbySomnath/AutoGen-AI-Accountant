19b4b09ba3972b6a26de31363b1f396fab9b4652
Somnath Kshirsagar <112457470+AIbySomnath@users.noreply.github.com>
2025-03-10 19:11:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ DEEPSEEK_API_BASE = "http://127.0.0.1:1234/v1"
 DEEPSEEK_API_KEY = "lm-studio"
 
 DB_CONFIG = {
-    "dbname": "your_database",
-    "user": "your_username",
-    "password": "your_password",
-    "host": "your_host",
+    "dbname": "mydb_x6y1",
+    "user": "mydb_x6y1_user",
+    "password": "pehRCAk2Xif0M8kQijE8WsupmX3g5KxV",
+    "host": "postgresql://mydb_x6y1_user:pehRCAk2Xif0M8kQijE8WsupmX3g5KxV@dpg-cv7elo0fnakc738kcbfg-a.virginia-postgres.render.com/mydb_x6y1",
     "port": "5432",
 }

554a35a2866a251949a578820fba13fff552851c
Somnath Kshirsagar <112457470+AIbySomnath@users.noreply.github.com>
2025-03-10 19:42:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ GROQ_API_BASE = "https://api.groq.com"
 GROQ_API_KEY = "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"
 
 DB_CONFIG = {
-    "dbname": "mydb_x6y1",
-    "user": "mydb_x6y1_user",
-    "password": "pehRCAk2Xif0M8kQijE8WsupmX3g5KxV",
-    "host": "postgresql://mydb_x6y1_user:pehRCAk2Xif0M8kQijE8WsupmX3g5KxV@dpg-cv7elo0fnakc738kcbfg-a.virginia-postgres.render.com/mydb_x6y1",
-    "port": "5432",
+    "dbname": "classicmodels",
+    "user": "root",
+    "password": "root",
+    "host": "localhost",
+    "port": "3306",
 }

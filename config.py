d51685b02422ad63cda89fc7ce50042f70674687
Somnath Kshirsagar <112457470+AIbySomnath@users.noreply.github.com>
2025-03-10 20:17:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,5 @@ GROQ_API_BASE = "https://api.groq.com"
 GROQ_API_KEY = "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"
 
 DB_CONFIG = {
-    "database": "classicmodels",
-    "user": "root",
-    "password": "root",
-    "host": "localhost",
-    "port": "3306",
+    "database_url": "sqlite:///finance.db"
 }
